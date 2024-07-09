03552e21395ee1ed3243ab287ff8da1a9dd0f3d1
cjustinobi <justin@Justins-MacBook-Pro.local>
2024-07-09 11:20:59 +0100

    WIP

--- src/components/shared/GameHeader.tsx
+++ src/components/shared/GameHeader.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Logo from '@/assets/img/logo.png'
 import Button from './Button'
 import { useDispatch } from 'react-redux'
+import Drawer from '../ui/Drawer'
 
 const GameHeader = () => {
   const dispatch = useDispatch()
@@ -38,25 +39,7 @@ const GameHeader = () => {
             </Button>
           </div>
         </div>
-
-        <button
-          type="button"
-          className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Menu
-        </button>
+        <Drawer />
       </header>
     </div>
   )
